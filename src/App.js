f0f24c045c7e912e0b4c2ddd5f819393161a4b80
Danilo Almeida <danilo_lima1993@hotmail.com>
2023-09-16 19:04:26 -0300

    Adicionando Material UI ao projeto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { Button } from "@mui/material";
 import "./App.css";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>Project</div>
+        <Button
+          variant="outlined"
+          startIcon={<LightbulbIcon />}
+          onClick={() => {
+            acenderLuz();
+          }}
+        >
+          Delete
+        </Button>
       </header>
     </div>
   );
+}
+
+function acenderLuz() {
+  console.log("teste teste");
 }
 
 export default App;
